4334c403dcb543df165a905340db7747269b40f6
TumayAltintas <100495086+TumayAltintas@users.noreply.github.com>
2023-01-23 00:57:22 +0300

    düzenlemeler

--- src/components/Search/SearchBack.vue
+++ src/components/Search/SearchBack.vue
@@ -83,7 +83,7 @@ input[type="text"] {
   background-position: 50% 50%;
   align-content: center;
   justify-content: center;
-  background-image: url('./src/components/photo/backgraound.png');
+  background-image: url('https://initiate.alphacoders.com/images/644/cropped-1300-300-644681.png?975');
 
 
 }
