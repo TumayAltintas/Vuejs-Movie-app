cbfa0a04508acc7b21c3510ae61206f165bf22ef
TumayAltintas <100495086+TumayAltintas@users.noreply.github.com>
2023-01-23 17:37:20 +0300

    Düzenlemler

--- src/components/Search/SearchBack.vue
+++ src/components/Search/SearchBack.vue
@@ -83,7 +83,7 @@ input[type="text"] {
   background-position: 50% 50%;
   align-content: center;
   justify-content: center;
-  background-image: url('https://www.hizliresim.com/86soejn');
+  background-image: url('./src/components/photo/backgraound.png');
 
 
 }
