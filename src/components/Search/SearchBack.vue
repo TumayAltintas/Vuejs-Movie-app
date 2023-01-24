4de8d71ae210c7e45248728bd6d3b72464223180
TumayAltintas <100495086+TumayAltintas@users.noreply.github.com>
2023-01-24 22:37:22 +0300

    Düzenlemler

--- src/components/Search/SearchBack.vue
+++ src/components/Search/SearchBack.vue
@@ -29,7 +29,7 @@ export default {
       searchText: "",
       random: Math.floor(Math.random() * 200),
       second: '',
-      image : "./src/components/photo/backgraound.png"
+      image : "../components/photo/backgraound.png"
     }
   },
   // async mounted() {
