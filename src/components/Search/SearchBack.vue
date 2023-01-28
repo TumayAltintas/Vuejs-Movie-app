a948a418b949e550eba791db58c0398ebffe4af5
TumayAltintas <100495086+TumayAltintas@users.noreply.github.com>
2023-01-28 18:01:10 +0300

    globaljs düzenleme

--- src/components/Search/SearchBack.vue
+++ src/components/Search/SearchBack.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="img-container container">
     <div class="img-c" style="background-image: url(src/components/photo/backgraound.jpg);">
+      <img src="src/components/photo/backgraound.jpg">
       <section class="warms">
         <div>
           <h2>Welcome.</h2>
